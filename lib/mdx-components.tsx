d4665a7bc4b8ee3eb9adad3807ff1a2d4c9f442d
Ming Feng KAM <kam_ming_feng@tech.gov.sg>
2026-05-27 19:40:51 +0800

    feat: add MDX component registry

---
+++ lib/mdx-components.tsx
@@ -0,0 +1,12 @@
+import type { MDXComponents } from 'mdx/types'
+import { ProsCons } from '@/components/pros-cons'
+import { PicksList } from '@/components/picks-list'
+import { ScoreBreakdown } from '@/components/score-breakdown'
+import { ComparisonTable } from '@/components/comparison-table'
+
+export const mdxComponents: MDXComponents = {
+  ProsCons,
+  PicksList,
+  ScoreBreakdown,
+  ComparisonTable,
+}
